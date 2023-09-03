7701aac81236005288a7568cada564e69c3e6577
CleanWater03 <nate03928@gmail.com>
2023-09-03 15:48:51 +0900

    0903

---
+++ section09/Ex09-1-builtin-function.py
@@ -0,0 +1,22 @@
+'''
+파일명 : Ex09-1-builtin-function.py
+
+내장함수
+    파이썬에서 제공해주는 별도의 import 없이
+    어디서든 사용할 수 있는 함수
+'''
+'''
+chr() 함수
+    아스키 코드를 문자로 반환하는 함수
+'''
+ch1 = chr(65)
+print(ch1)
+
+'''
+eval() 함수
+    매개변수로 받은 expression(=식)을
+    문자열로 받아서 실행하는 함수
+'''
+expr = input('계산식을 입력하세요 >>> ')
+result = eval(expr)
+print(f'{expr}={result}')
