32e306b10574eb52ddfdba2f2902da5d66e549f6
CleanWater03 <nate03928@gmail.com>
2023-09-19 19:57:32 +0900

    0916

---
+++ section18/Ex18-7-selenium-googleImages.py
@@ -0,0 +1,13 @@
+'''
+파일명 : Ex18-7-selenium-googleImages
+
+selenium 패키지
+    애플리케이션 테스트를 하기 위한 프레임웍 입닏다.
+    웹 애플리케이션 다양한 브라우저 동작 테스트용
+    웹 크롤링으로 많이 사용된다.
+    java, python, c#, ruby 등 다양한 언어 지원
+
+https://chromedriver.chromium.org/downloads
+
+크롬 우측 상단 ... -> 도움말
+'''
