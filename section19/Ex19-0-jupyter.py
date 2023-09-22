bf1f1b233450159e5ae392b621f026c7af7943fb
CleanWater03 <nate03928@gmail.com>
2023-09-22 12:05:33 +0900

    Changes

---
+++ section19/Ex19-0-jupyter.py
@@ -0,0 +1,30 @@
+'''
+파일명 : Ex19-0-jupyter.py
+
+설치방법
+    pip install jupyter
+
+실행방법
+    jupyter notebook
+
+
+명령 모드(활성화 하려면 ESC 키 누름)
+Shift-Enter : 셀 실행, 아래 선택
+Ctrl-Enter : 셀 실행
+Alt-Enter : 셀 실행, 아래에 삽입
+y : 코딩
+m : 마크다운으로
+a : 위에 셀 삽입
+b : 아래에 셀 삽입
+x : 선택한 셀 잘라내기
+c : 선택한 셀 복사
+Shift-v : 위에 셀 붙여넣기
+v : 아래에 셀 붙여넣기
+z
+d,d : 선택한 셀 삭제
+Shift-M : 아래 셀 병합
+편집 모드 (활성화 하려면 Enter 키 를 누름)
+Shift-Tab : 툴팁표시
+Ctrl-] : 들여쓰기
+Ctrl-Shift- : 셀분할
+'''
